a072bd1deff88c860851c6edd1389e70254c639f
Felipe Romao <feliperomaocad@gmail.com>
2018-10-14 23:10:18 -0300

    Implementando pesquisa e teclas de atalho

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -6,6 +6,14 @@ import IconButton from '../template/iconButton'
 export default class TodoForm extends Component{
   constructor(props){
     super(props)
+    this.keyHandle = this.keyHandle.bind(this)
+  }
+  keyHandle(e){
+    if( e.key === 'Enter' ){
+      e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+    }else if( e.key === 'Escape' ){
+      this.props.handleClean()
+    }
   }
   render(){
     return(
@@ -14,12 +22,15 @@ export default class TodoForm extends Component{
           <input
           value={ this.props.description }
           onChange={ (value) => this.props.handleChange(value) }
+          onKeyUp={ this.keyHandle }
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"></input>
         </Grid>
         <Grid cols="12 3 2">
           <IconButton style="primary" icon="plus" onClick={ this.props.handleAdd } />
+          <IconButton style="info" icon="search" onClick={ this.props.handleSearch } />
+          <IconButton style="default" icon="close" onClick={ this.props.handleClean } />
         </Grid>
       </div>
     )
--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,32 +1,36 @@
 import React, { Component } from 'react'
 
 import IconButton from '../template/iconButton'
+import If from '../template/if'
 
-export default class TodoList extends Component{
-  constructor(props){
+export default class TodoList extends Component {
+  constructor(props) {
     super(props)
   }
-  renderRows(){
+  renderRows() {
     const list = this.props.list || []
-    return list.map( todo => (
-      <tr key={ todo._id }>
-        <td>{ todo.description }</td>
-        <td>
-          <IconButton style="danger" icon="trash-o" onClick={ () => this.props.handleRemove(todo) } />
+    return list.map(todo => (
+      <tr className={todo.done ? "checkedAsDone" : ""} key={todo._id}>
+        <td>{todo.description}</td>
+        <td className="tableActions">
+          <IconButton style="success" icon="check" hide={ todo.done } onClick={() => this.props.handleCheckDone(todo)} />
+          <IconButton style="warning" icon="undo" hide={ !todo.done } onClick={() => this.props.handleCheckUndone(todo)} />
+          <IconButton style="danger" icon="trash-o" onClick={() => this.props.handleRemove(todo)} />
         </td>
       </tr>
     ))
   }
-  render(){
-    return(
+  render() {
+    return (
       <table className="table">
         <thead>
           <tr>
             <th>Descrição</th>
+            <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          { this.renderRows() }
+          {this.renderRows()}
         </tbody>
       </table>
     )
